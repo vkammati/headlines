073cc2fad4f3feae2b87bb364554332f953a0d7b
vkammati <kvk5592007@gmail.com>
2022-08-24 18:58:18 +0530

    Using GET

--- flask_url_routing.py
+++ flask_url_routing.py
@@ -1,25 +1,23 @@
 import feedparser
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
-'cnn': 'http://rss.cnn.com/rss/edition.rss',
-'fox': 'http://feeds.foxnews.com/foxnews/latest',
-'iol': 'http://www.iol.co.za/cmlink/1.640'}
+            'cnn': 'http://rss.cnn.com/rss/edition.rss',
+            'fox': 'http://feeds.foxnews.com/foxnews/latest',
+            'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-@app.route("/bbc")
-def bbc():
-    return get_news('bbc')
 
-@app.route("/cnn")
-def cnn():
-    return get_news('cnn')
-
-def get_news(publication):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html",articles=feed['entries'])
+    return render_template("home.html", articles=feed['entries'])
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
